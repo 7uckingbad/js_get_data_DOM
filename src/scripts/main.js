7b4e1522f677d506fdf636b737f21d50a2e665e7
Denis <denismushta@gmail.com>
2025-08-16 12:45:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+
+const averageCount = document.querySelectorAll('.population');
+
+let total = 0;
+
+averageCount.forEach((span) => {
+  const number = Number(span.textContent.replaceAll(',', ''));
+
+  total += number;
+});
+
+const totalCount = total / averageCount.length;
+
+document.querySelector('.total-population').textContent =
+  total.toLocaleString();
+
+document.querySelector('.average-population').textContent =
+  totalCount.toLocaleString();
